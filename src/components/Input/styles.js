9f3659312c1871ace49b398767775ff7b29abbc5
Juliana Ramos <cjota.ramos@gmail.com>
2024-08-24 23:25:05 -0300

    primeiro commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled  from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size:24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAFFFF;
+        border: 0;
+        display: block;
+        direction: ltr;
+
+        font-size:24px;
+        font-family: 'Roboto';
+    }
+`
